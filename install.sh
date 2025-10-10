93f04ff0f37d198422ed1d9626a09dfda16ffebf
Sergio Sanchez <ssancd03@estudiantes.unileon.es>
2025-10-10 11:35:34 +0200

    Updated install

--- install.sh
+++ install.sh
@@ -54,3 +54,39 @@ echo -e "${GREEN}========================${RESET}"
 wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -y ./google-chrome-stable_current_amd64.deb
 rm -f google-chrome-stable_current_amd64.deb
+
+read -p "Press Enter to continue..."
+clear
+
+echo -e "${BLUE}========================${RESET}"
+echo -e "${BLUE}  Installing Docker     ${RESET}"
+echo -e "${BLUE}========================${RESET}"
+apt install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install -y docker-ce
+groupmod -g 1000 docker
+
+read -p "Press Enter to continue..."
+clear
+
+echo -e "${YELLOW}========================${RESET}"
+echo -e "${YELLOW} Configuring Defaults   ${RESET}"
+echo -e "${YELLOW}========================${RESET}"
+cp conf/90_ubuntu-settings.gschema.override /usr/share/glib-2.0/schemas/
+glib-compile-schemas /usr/share/glib-2.0/schemas
+
+read -p "Press Enter to continue..."
+clear
+
+echo -e "${CYAN}========================${RESET}"
+echo -e "${CYAN} Configuring .bashrc    ${RESET}"
+echo -e "${CYAN}========================${RESET}"
+echo "source /opt/ros/jazzy/setup.bash" >> /etc/skel/.bashrc
+
+echo -e "${GREEN}========================${RESET}"
+echo -e "${GREEN} Install complete!      ${RESET}"
+echo -e "${GREEN}========================${RESET}"
