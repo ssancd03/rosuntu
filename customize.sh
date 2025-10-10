bec7662ea0e3e4b8d7cbb2398dc1ada33b46e65a
Sergio Sanchez <ssancd03@estudiantes.unileon.es>
2025-10-10 11:41:23 +0200

    updated custom

--- customize.sh
+++ customize.sh
@@ -3,6 +3,12 @@ set -e
 
 echo "Configuring favorite apps in dock..."
 
-dconf write /org/gnome/shell/favorite-apps "['google-chrome.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'code.desktop']"
+mkdir -p /etc/dconf/db/local.d
+cat > /etc/dconf/db/local.d/01-favorite-apps << 'EOF'
+[org/gnome/shell]
+favorite-apps=['google-chrome.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'code.desktop']
+EOF
+
+dconf update
 
 echo "Dock favorites configured successfully!"
